2fb308711f61eb953884dcf23f5cd7ee9c92e0cc
DevoutPrayer <597707323@qq.com>
2021-02-09 19:37:32 +0800

    project init

---
+++ Drivers/P30/P30.cpp
@@ -0,0 +1,18 @@
+/*
+ * P30.cpp
+ *
+ *  Created on: Feb 9, 2021
+ *      Author: DevoutPrayer
+ */
+
+#include "P30.h"
+
+P30::P30() {
+	// TODO Auto-generated constructor stub
+
+}
+
+P30::~P30() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Drivers/P30/P30.h
@@ -0,0 +1,17 @@
+/*
+ * P30.h
+ *
+ *  Created on: Feb 9, 2021
+ *      Author: DevoutPrayer
+ */
+
+#ifndef P30_P30_H_
+#define P30_P30_H_
+
+class P30 {
+public:
+	P30();
+	virtual ~P30();
+};
+
+#endif /* P30_P30_H_ */
